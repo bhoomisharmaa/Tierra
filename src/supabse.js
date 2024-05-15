e9086ffbaee2c949eb4daab19d357fa45b48f441
bhoomisharmaa <bhoomisharma404@gmail.com>
2024-05-15 18:54:31 +0530

    removed supabase

--- src/supabse.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://tzvuzbvmplsmsjfunyiu.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6dnV6YnZtcGxzbXNqZnVueWl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU0NTU5NDIsImV4cCI6MjAzMTAzMTk0Mn0.Pz21HnrAZh3YMo02mIgxycG3h92oAUX07R03B3EE_4M";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
